0ab6e8ef849c32a9506d638d89336b8460b20d46
taucia <taucia.jackson123@gmail.com>
2022-06-29 15:36:12 +0200

    change

--- script/code.js
+++ script/code.js
@@ -13,7 +13,7 @@ function updatemenu() {
             id: 1,
             type: 'Apartment',
             location: 'New York City',
-            url: "https://i.postimg.cc/65Ms5MwD/Luxury-Apartment.jpg",
+            url: "../image/00a19f3f8242329547c008f419bead41.jpg",
             bed: `<i class="fa-solid fa-bed"> 4 </i>`,
             toilet: `<i class="fa-solid fa-toilet"> 2</i>`,
             price: 10000
@@ -22,7 +22,7 @@ function updatemenu() {
             id: 2,
             type: 'Apartment',
             location: 'New York City',
-            url: "https://i.postimg.cc/65Ms5MwD/Luxury-Apartment.jpg",
+            url: "../image/20072093-1-a3b6dda0b39bd0968ab4e7b28a350cc3-m.jpg",
             bed: '<i class="fa-solid fa-bed"> 3</i>',
             toilet: `<i class="fa-solid fa-toilet"> 2</i>`,
             price: 10000
@@ -31,7 +31,7 @@ function updatemenu() {
             id: 3,
             type: 'Apartment',
             location: 'New York City',
-            url: "https://i.postimg.cc/65Ms5MwD/Luxury-Apartment.jpg",
+            url: "https://i.postimg.cc/SK3xW5dY/21f59207f31a1be0657c9b10492ed527.jpg",
             bed: '<i class="fa-solid fa-bed"> 2</i>',
             toilet: `<i class="fa-solid fa-toilet"> 2</i>`,
             price: 10000
@@ -40,7 +40,7 @@ function updatemenu() {
             id: 4,
             type: 'Apartment',
             location: 'New York City',
-            url: "https://i.postimg.cc/65Ms5MwD/Luxury-Apartment.jpg",
+            url: "../image/5b687231639838184bda69a5a748282c.jpg",
             bed: '<i class="fa-solid fa-bed"> 5</i>',
             toilet: `<i class="fa-solid fa-toilet"> 3</i>`,
             price: 10000
@@ -49,7 +49,7 @@ function updatemenu() {
             id: 5,
             type: 'Apartment',
             location: 'New York City',
-            url: "https://i.postimg.cc/65Ms5MwD/Luxury-Apartment.jpg",
+            url: "../image/cbce6718be5470d2e4e4673dae2a2c41.jpg",
             bed: '<i class="fa-solid fa-bed"> 3</i>',
             toilet: `<i class="fa-solid fa-toilet"> 2</i>`,
             price: 10000
@@ -58,11 +58,64 @@ function updatemenu() {
             id: 6,
             type: 'Apartment',
             location: 'New York City',
-            url: "https://i.postimg.cc/65Ms5MwD/Luxury-Apartment.jpg",
+            url: "../image/ccd5ccd6c88be5bb4e3fa256e5798478.jpg",
             bed: '<i class="fa-solid fa-bed"> 3</i>',
             toilet: `<i class="fa-solid fa-toilet"> 2</i>`,
             price: 10000
-        }
+        },
+        {
+          id: 7,
+          type: 'Apartment',
+          location: 'New York City',
+          url: "https://i.postimg.cc/zvCXKYJD/7.jpg",
+          bed: '<i class="fa-solid fa-bed"> 3</i>',
+          toilet: `<i class="fa-solid fa-toilet"> 2</i>`,
+          price: 10000
+      },
+      {
+        id: 8,
+        type: 'Apartment',
+        location: 'New York City',
+        url: "../image/images.jpg",
+        bed: '<i class="fa-solid fa-bed"> 3</i>',
+        toilet: `<i class="fa-solid fa-toilet"> 2</i>`,
+        price: 10000
+    },
+    {
+      id: 9,
+      type: 'Apartment',
+      location: 'New York City',
+      url: "../image/taylor-uptown-penthouse-unit-333-dallas-tx-building-photo.jpg",
+      bed: '<i class="fa-solid fa-bed"> 3</i>',
+      toilet: `<i class="fa-solid fa-toilet"> 2</i>`,
+      price: 10000
+  },     {
+    id: 10,
+    type: 'Apartment',
+    location: 'New York City',
+    url: "https://i.postimg.cc/GpL3D5Nd/8.jpg",
+    bed: '<i class="fa-solid fa-bed"> 3</i>',
+    toilet: `<i class="fa-solid fa-toilet"> 2</i>`,
+    price: 10000
+},
+  {
+    id: 11,
+    type: 'Apartment',
+    location: 'New York City',
+    url: "../image/99.jpg",
+    bed: '<i class="fa-solid fa-bed"> 3</i>',
+    toilet: `<i class="fa-solid fa-toilet"> 2</i>`,
+    price: 10000
+},
+{
+  id: 12,
+  type: 'Apartment',
+  location: 'New York City',
+  url: "../image/10.jpg.crdownload",
+  bed: '<i class="fa-solid fa-bed"> 3</i>',
+  toilet: `<i class="fa-solid fa-toilet"> 2</i>`,
+  price: 10000
+}
     ];
 
 
